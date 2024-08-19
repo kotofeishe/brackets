7eae1b7d3d8eeb1085c3d3dcd3e862b5941983f1
Kotofeishe <ksenia.migunova91@yandex.ru>
2024-08-19 18:44:24 +0300

    well done task: check brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  let newStr = str;
+  let i = 0 ;
+  while (i < newStr.length) {
+    k = newStr.length;
+    bracketsConfig.forEach(value => {
+      if (value[0] === newStr[i] && value[1] === newStr[i+1]) {
+        deleteEl = newStr[i]+newStr[i+1];
+        newStr = newStr.split(deleteEl).join('');
+        i = 0;
+      }
+    })
+    if (newStr.length === k ) {
+      i = i + 1;
+    }
+  }
+  if (newStr.length === 0) {
+    return true;
+  }
+  else {return false;}
+
+  }
